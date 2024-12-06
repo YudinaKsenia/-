4513b6e0e03d36066eba680eb0e9f651e4a37b5f
KseniaYudina <uksenia359@gmail.com>
2024-12-06 15:49:43 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 16890;
+        int amountForBonus = 20;
+
+        int bonusMiles = cost / amountForBonus;
+
+        System.out.println(bonusMiles);
+    }
+}
